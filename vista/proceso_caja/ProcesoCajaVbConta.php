12126a5e35aa6e5e327606c872b0695216020bb0
maylee <mayperez2010@gmail.com>
2018-06-25 17:35:39 -0400

    filtro años en interfaz de solicitudEfectivo

--- vista/proceso_caja/ProcesoCajaVbConta.php
+++ vista/proceso_caja/ProcesoCajaVbConta.php
@@ -1,114 +1,186 @@
 <?php
 /**
-*@package pXP
-*@file ProcesoCajaVbConta.php
-*@author  Gonzalo Sarmiento Sejas
-*@date 24-12-2015
-*@description Archivo con la interfaz de usuario que permite
-*dar el visto a Rendiciones y Reposiciones en Contabilidad
-*
-*/
+ * @package pXP
+ * @file ProcesoCajaVbConta.php
+ * @author  Gonzalo Sarmiento Sejas
+ * @date 24-12-2015
+ * @description Archivo con la interfaz de usuario que permite
+ *dar el visto a Rendiciones y Reposiciones en Contabilidad
+ *
+ */
 header("content-type: text/javascript; charset=UTF-8");
 ?>
 <script>
-Phx.vista.ProcesoCajaVbConta = {
-    bedit:false,
-    bnew:false,
-    bsave:false,
-    bdel:false,
-	require:'../../../sis_tesoreria/vista/proceso_caja/ProcesoCajaVbPresup.php',
-	requireclase:'Phx.vista.ProcesoCajaVbPresup',
-	title:'Visto Bueno Rendiciones Caja Contabilidad',
-	nombreVista: 'ProcesoCajaVbConta',
-	/*
-	 *  Interface heredada en el sistema de tesoreria para que el responsable
-	 *  de rendiciones apruebe las rendiciones , y pase por los pasos configurados en el WF
-	 *  de validacion, aprobacion
-	 * */
-	 
-	gruposBarraTareas:[{name:'no_contabilizados',title:'<H1 align="center"><i class="fa fa-thumbs-o-down"></i> No Contabilizados</h1>',grupo:0,height:0},
-                       {name:'contabilizados',title:'<H1 align="center"><i class="fa fa-thumbs-o-up"></i> Contabilizados</h1>',grupo:1,height:0}],
-	
-	actualizarSegunTab: function(name, indice){
-		
-    	if(this.finCons){
-    		 this.store.baseParams.pes_estado = name;
-    	     this.load({params:{start:0, limit:this.tam_pag, tipo_interfaz: this.nombreVista}});
-    	   }
-    },
+    Phx.vista.ProcesoCajaVbConta = {
+        bedit: false,
+        bnew: false,
+        bsave: false,
+        bdel: false,
+        require: '../../../sis_tesoreria/vista/proceso_caja/ProcesoCajaVbPresup.php',
+        requireclase: 'Phx.vista.ProcesoCajaVbPresup',
+        title: 'Visto Bueno Rendiciones Caja Contabilidad',
+        nombreVista: 'ProcesoCajaVbConta',
+        /*
+         *  Interface heredada en el sistema de tesoreria para que el responsable
+         *  de rendiciones apruebe las rendiciones , y pase por los pasos configurados en el WF
+         *  de validacion, aprobacion
+         * */
 
-	bactGroups:  [0,1],
-	
-	constructor: function(config) {
+        gruposBarraTareas: [{
+            name: 'no_contabilizados',
+            title: '<H1 align="center"><i class="fa fa-thumbs-o-down"></i> No Contabilizados</h1>',
+            grupo: 0,
+            height: 0
+        },
+            {
+                name: 'contabilizados',
+                title: '<H1 align="center"><i class="fa fa-thumbs-o-up"></i> Contabilizados</h1>',
+                grupo: 1,
+                height: 0
+            }],
 
-	   this.finCons = true;
+        actualizarSegunTab: function (name, indice) {
 
-	   Phx.vista.ProcesoCajaVbConta.superclass.constructor.call(this,config);
+            if (this.finCons) {
+                this.store.baseParams.pes_estado = name;
+                this.load({params: {start: 0, limit: this.tam_pag, tipo_interfaz: this.nombreVista}});
+            }
+        },
 
-    },
-	
-	preparaMenu:function(){
-	   var data = this.getSelectedData();
-	   Phx.vista.ProcesoCajaVbConta.superclass.preparaMenu.call(this);
-		if(data['tipo']=='CIERRE'){
-			  //habilitar pestaña depositos
-			  this.enableTabDepositos();
-		  }else{
-			  //deshabilitar pestaña depositos
-			  this.disableTabDepositos();
-		  }
-		if(data['nombre'] == 'Solo Rendir'){
-			this.getBoton('chkpresupuesto').enable();
-			this.getBoton('consolidado_rendicion').enable();
-		}else{
-			this.getBoton('chkpresupuesto').disable();
-			this.getBoton('consolidado_rendicion').disable();
-		}
-	},
+        bactGroups: [0, 1],
 
-	checkPresupuesto:function(){
-		var rec=this.sm.getSelected();
-		var configExtra = [];
-		if (rec.data.estado == 'rendido') {
-			this.objChkPres = Phx.CP.loadWindows('../../../sis_presupuestos/vista/presup_partida/ChkPresupuesto.php',
-					'Estado del Presupuesto',
-					{
-						modal: true,
-						width: 700,
-						height: 450
-					}, {
-						data: {
-							nro_tramite: rec.data.nro_tramite
-						}
-					}, this.idContenedor, 'ChkPresupuesto',
-					{
-						config: [{
-							event: 'onclose',
-							delegate: this.onCloseChk
-						}],
+        constructor: function (config) {
 
-						scope: this
-					});
-		}else{
-			this.objChkPres = Phx.CP.loadWindows('../../../sis_presupuestos/vista/presup_partida/ChkPresupuestoRendicion.php',
-					'Estado del Presupuesto',
-					{
-						modal:true,
-						width:700,
-						height:450
-					}, {
-						data:{
-							nro_tramite: rec.data.nro_tramite
-						}}, this.idContenedor,'ChkPresupuesto',
-					{
-						config:[{
-							event:'onclose',
-							delegate: this.onCloseChk
-						}],
+            this.tbarItems = ['-',
+                'Gestión:', this.cmbGestion, '-'
+            ];
+            var fecha = new Date();
+            Ext.Ajax.request({
+                url: '../../sis_parametros/control/Gestion/obtenerGestionByFecha',
+                params: {fecha: fecha.getDate() + '/' + (fecha.getMonth() + 1) + '/' + fecha.getFullYear()},
+                success: function (resp) {
+                    var reg = Ext.decode(Ext.util.Format.trim(resp.responseText));
+                    this.cmbGestion.setValue(reg.ROOT.datos.id_gestion);
+                    this.cmbGestion.setRawValue(fecha.getFullYear());
+                    this.store.baseParams.id_gestion = reg.ROOT.datos.id_gestion;
+                    this.load({params: {start: 0, limit: this.tam_pag}});
+                },
+                failure: this.conexionFailure,
+                timeout: this.timeout,
+                scope: this
+            });
 
-						scope:this
-					});
-		}
-	}
-};
+            this.finCons = true;
+
+            Phx.vista.ProcesoCajaVbConta.superclass.constructor.call(this, config);
+
+            this.cmbGestion.on('select', this.capturarEventos, this);
+        },
+
+        cmbGestion: new Ext.form.ComboBox({
+            //name: 'gestion',
+            // id: 'gestion_reg',
+            fieldLabel: 'Gestion',
+            allowBlank: true,
+            emptyText: 'Gestion...',
+            blankText: 'Año',
+            editable: false,
+            store: new Ext.data.JsonStore(
+                {
+                    url: '../../sis_parametros/control/Gestion/listarGestion',
+                    id: 'id_gestion',
+                    root: 'datos',
+                    sortInfo: {
+                        field: 'gestion',
+                        direction: 'DESC'
+                    },
+                    totalProperty: 'total',
+                    fields: ['id_gestion', 'gestion'],
+                    // turn on remote sorting
+                    remoteSort: true,
+                    baseParams: {par_filtro: 'gestion'}
+                }),
+            valueField: 'id_gestion',
+            triggerAction: 'all',
+            displayField: 'gestion',
+            hiddenName: 'id_gestion',
+            mode: 'remote',
+            pageSize: 5,
+            queryDelay: 500,
+            listWidth: '280',
+            hidden: false,
+            width: 80
+        }),
+
+        capturarEventos: function () {
+
+            this.store.baseParams.id_gestion = this.cmbGestion.getValue();
+            this.load({params: {start: 0, limit: this.tam_pag}});
+        },
+
+        preparaMenu: function () {
+            var data = this.getSelectedData();
+            Phx.vista.ProcesoCajaVbConta.superclass.preparaMenu.call(this);
+            if (data['tipo'] == 'CIERRE') {
+                //habilitar pestaña depositos
+                this.enableTabDepositos();
+            } else {
+                //deshabilitar pestaña depositos
+                this.disableTabDepositos();
+            }
+            if (data['nombre'] == 'Solo Rendir') {
+                this.getBoton('chkpresupuesto').enable();
+                this.getBoton('consolidado_rendicion').enable();
+            } else {
+                this.getBoton('chkpresupuesto').disable();
+                this.getBoton('consolidado_rendicion').disable();
+            }
+        },
+
+        checkPresupuesto: function () {
+            var rec = this.sm.getSelected();
+            var configExtra = [];
+            if (rec.data.estado == 'rendido') {
+                this.objChkPres = Phx.CP.loadWindows('../../../sis_presupuestos/vista/presup_partida/ChkPresupuesto.php',
+                    'Estado del Presupuesto',
+                    {
+                        modal: true,
+                        width: 700,
+                        height: 450
+                    }, {
+                        data: {
+                            nro_tramite: rec.data.nro_tramite
+                        }
+                    }, this.idContenedor, 'ChkPresupuesto',
+                    {
+                        config: [{
+                            event: 'onclose',
+                            delegate: this.onCloseChk
+                        }],
+
+                        scope: this
+                    });
+            } else {
+                this.objChkPres = Phx.CP.loadWindows('../../../sis_presupuestos/vista/presup_partida/ChkPresupuestoRendicion.php',
+                    'Estado del Presupuesto',
+                    {
+                        modal: true,
+                        width: 700,
+                        height: 450
+                    }, {
+                        data: {
+                            nro_tramite: rec.data.nro_tramite
+                        }
+                    }, this.idContenedor, 'ChkPresupuesto',
+                    {
+                        config: [{
+                            event: 'onclose',
+                            delegate: this.onCloseChk
+                        }],
+
+                        scope: this
+                    });
+            }
+        }
+    };
 </script>
